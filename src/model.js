d5f0e80fd28f960064f864f70c68cb8d6f257026
Vito <topvetal@gmail.com>
2021-02-14 23:44:20 +0200

    Revert "1st_comm"
    This reverts commit e5632ddca76365a3fdd17cdcb2410af40a004464.

--- src/model.js
+++
@@ -1,8 +0,0 @@
-import image from './assets/image.png'
-
-export const model = [
-  { type: "title", value: "Hello World from JS" },
-  { type: "text", value: "here we go with some text" },
-  { type: "columns", value: ["1111", "2222222", "33333333", '55555'] },
-  { type: "image", value: image }
-];
